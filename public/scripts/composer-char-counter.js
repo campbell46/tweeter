6b4d323b90434a04ea9e7969560acf62584569d1
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-11-13 21:14:25 -0500

    Create function to count tweet length, display red if < 0

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  const maxCount = $('output.counter').val();
+
+  $("#tweet-text").keyup(function() {
+    const textLength = $(this).val().length;
+    const counter = $('output.counter');
+
+    counter.val(maxCount - textLength);
+
+    if (counter.val() < 0) {
+      counter.addClass("red-font");
+    } else if (counter.val() > -1) {
+      counter.removeClass("red-font");
+    }
+  });
+});
